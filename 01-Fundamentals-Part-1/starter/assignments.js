14eead0ff2f7ffa18677b942654753ebab010b16
Willy Cass <1844021+wcass77@users.noreply.github.com>
2023-04-16 19:39:30 -0400

    Assigment 2 - 12

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,19 @@
+console.log("\n-------------------------------\n\n");
+//Assignment 1
 let country = 'USA';
 let continent = 'North America';
 let population = 300 * 10 ** 6;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+//Assignment 2
+let isIsland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
