d3dc601867dac9c0d5dd3ac725b86a7749248fa7
Willy Cass <1844021+wcass77@users.noreply.github.com>
2023-04-16 19:39:09 -0400

    Lecture 2-12

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,7 @@
+/*
+// Initial Example
 let js = 'amazing';
-console.log(40 + 7 + 23 - 10);
+// console.log(40 + 7 + 23 - 10);
 
 console.log('Jonas');
 console.log(23);
@@ -9,7 +11,9 @@ let firstName = "Willy";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-
+*/
+/*
+// Variable Naming
 //let 23andMe = 'no';         //Can't start with letter
 //let jonas& matilda = "JM;   //Can't use symbols except $ and _
 //let function = "function" ";  //reserved word
@@ -25,3 +29,27 @@ let job1 = "teacher";
 let job2 = "programmer";
 
 console.log(myFirstJob);
+*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+console.log(typeof "jonas");
+
+//reassignment to another type
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); //prints object, which is a bug left for legacy reasons
