c648e64fa8ad4dbd310868c535266be737c3f9f4
Willy Cass <1844021+wcass77@users.noreply.github.com>
2023-04-16 17:37:32 -0400

    Lecture 2-10: Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,27 @@
 let js = 'amazing';
-if (js == 'amazing') alert('Javascript is FUN!')
-
 console.log(40 + 7 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Willy";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//let 23andMe = 'no';         //Can't start with letter
+//let jonas& matilda = "JM;   //Can't use symbols except $ and _
+//let function = "function" ";  //reserved word
+
+let name = "Me"; //works but causes problems
+let Person = 'Jonas' //uppercase only for classes
+let PI = 3.14;  //upper case for constants
+
+let myFirstJob = "teacher";
+let myCurrentJob = "programmer";
+
+let job1 = "teacher";
+let job2 = "programmer";
+
+console.log(MediaQueryListEvent);
