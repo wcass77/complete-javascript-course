198722ff4bb4a9618a557d82d7142cba4cf2a4d9
Willy Cass <1844021+wcass77@users.noreply.github.com>
2023-04-16 21:01:16 -0400

    Lecture 2-14

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -56,6 +56,7 @@ console.log(typeof year);
 console.log(typeof null); //prints object, which is a bug left for legacy reasons
 */
 
+/*
 let age = 30;
 age = 31; //"mutate" a variable
 
@@ -74,3 +75,34 @@ job = 'teacher';
 //Don't have to declare variables:
 lastName = 'Cass'; //GLOBAL!!
 console.log(lastName)
+*/
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2);
+console.log(ageJonas / 10, 2 ** 3);
+
+const firstName = 'Willy';
+const lastName = 'Cass';
+console.log(firstName + ' ' + lastName);
+
+//assignment operators
+let x = 10 + 5;
+console.log(x);
+x += 10; //x = x + 10
+x *= 4; //x = x * 4
+x++; //x = x + 1
+x--;
+x--;
+console.log(x);
+
+//comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018); //same as line 103
